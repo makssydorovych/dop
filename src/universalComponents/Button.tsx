89c70e4092d46b015cb81b3e0cbad1240a8cd1e5
Maksym <maksymsydorovych@gmail.com>
2022-09-14 14:18:31 +0200

    1

---
+++ src/universalComponents/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type PropsType={
+    name: string,
+    callBack:()=>void
+}
+export const Button=(props: PropsType)=> {
+    const{name,callBack}=props
+    const onClickHandler =()=>{
+    callBack()
+    }
+return(
+    <button onclick={onClickHandler}>{name}</button>
+)
+
+}
